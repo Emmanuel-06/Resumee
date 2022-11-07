8a0c464db9633b6558e89d1b819a83f9d06b5c84
enemona06 <enemonaemmanuel4@gmail.com>
2022-11-07 04:34:33 +0100

    initial commit

---
+++ app/src/main/java/com/example/resumee/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.resumee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.resumee.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
